91919e75393a3a9ca9a7d42f94323f845302869a
Alexander Komla <kunalexander360@gmail.com>
2025-03-04 09:14:53 +0000

    Create train and val splits

--- nlp_text_classification.py
+++ nlp_text_classification.py
@@ -1,6 +1,18 @@
 import pandas as pd 
 import numpy as np 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+from nltk.tokenize import word_tokenize 
+from nltk.corpus import stopwords 
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer  
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.model_selection import train_test_split
+
+import torch 
+from torch import nn 
+from torch.utils.data import DataLoader
+
+
 
 """Download and Prepare Data for Training"""
 
@@ -12,8 +24,42 @@ SAMPLE_SIZE = 100_000
 
 sample_df = raw_df.sample(SAMPLE_SIZE, random_state=42) 
 
-sample_df.target.value_counts(normalize=True).plot(kind="bar")
-plt.show()
+#sample_df.target.value_counts(normalize=True).plot(kind="bar")
+#plt.show() 
+
+"""
+Prepare Data for Training
+- Convert text to TF-IDF vectors
+- Convert vectors to PyTorch tensors
+- Create PyTorch Data Loaders
+"""
+english_stopwords = stopwords.words("english")
+stemmer = SnowballStemmer(language="english")
+def tokenize(text): 
+    return [stemmer.stem(token) for token in word_tokenize(text)]
+vectorizer = TfidfVectorizer(
+    lowercase=True, 
+    stop_words=english_stopwords, 
+    tokenizer= tokenize, 
+    max_features=1000
+) 
+
+vectorizer.fit(sample_df.question_text) 
+
+inputs = vectorizer.transform(sample_df.question_text)
+test_inputs = vectorizer.transform(test_df.question_text) 
+
+"""Split the training and validation sets"""
+train_inputs, val_inputs, train_targets, val_targets = train_test_split(
+    inputs, 
+    raw_df.target, 
+    test_size=0.3
+)
+
+print(len(train_inputs))
+print(len(val_inputs.shape))
+print(len(train_targets.shape))
+print(len(val_targets))
 
 
 
@@ -27,6 +73,4 @@ plt.show()
 
 
 
-
-
-"""Train Deep Learning Model"""
+"""Train Deep Learning Model""" 
